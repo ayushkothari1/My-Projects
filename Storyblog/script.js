f7c4b72b96ba3aaec911207c9f4909b813d732d9
ayushkothari1 <pahadiayush61@gmail.com>
2025-04-11 17:24:35 +0530

    storyextra

--- Storyblog/script.js
+++ Storyblog/script.js
@@ -3,5 +3,5 @@ const hide = document.querySelector(".hide");
 
 hide.addEventListener("click", function () {
   hide.style.display = "none";
-  show.style.display = "block";
+  show.style.display = "inline";
 });
