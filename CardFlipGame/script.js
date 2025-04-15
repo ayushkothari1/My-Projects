beb5dc387a8161d7425c9a85f075ac37f8560bc9
ayushkothari1 <pahadiayush61@gmail.com>
2025-04-15 14:39:38 +0530

    another

--- CardFlipGame/script.js
+++ CardFlipGame/script.js
@@ -1,9 +1,25 @@
+const imagList = [
+  "/images/th (1).jpg",
+  "/images/th (2).jpg",
+  "/images/th (3).jpg",
+  "/images/th.jpg",
+];
 const mainImg = document.querySelector("#main-img");
 const redImg = document.querySelector("#red");
 const options = document.querySelectorAll(".option-img");
+function changeMainImage() {
+  const randoImage = Math.floor(Math.random() * imagList.length);
+  mainImg.setAttribute("src", imagList[randoImage]);
+}
+changeMainImage();
 options.forEach(function (e) {
-  e.addEventListener("click", function () {
+  e.addEventListener("click", function (t) {
     redImg.style.display = "none";
     mainImg.classList.add("active");
+    if (t.target.getAttribute("src") === mainImg.getAttribute("src")) {
+      console.log("hey you got it man");
+    } else {
+      console.log("fuck you loser");
+    }
   });
 });
