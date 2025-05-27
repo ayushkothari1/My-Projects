9865169f9a01752d27171f80c637073398d86e5e
ayushkothari1 <pahadiayush61@gmail.com>
2025-05-27 14:20:34 +0530

    extra

---
+++ Timer/pscript.js
@@ -0,0 +1,48 @@
+const startEl = document.querySelector("#start");
+const stopEl = document.querySelector("#stop");
+const resetEl = document.querySelector("#reset");
+const timeEl = document.querySelector("#time");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+
+let timer = null;
+let m = 25;
+let s = 0;
+const num = (n) => (n < 10 ? "0" + n : n);
+function updateDisplay() {
+  minuteEl.innerHTML = num(m);
+  secondEl.innerHTML = num(s);
+}
+
+startEl.addEventListener("click", function () {
+  if (timer !== null) return;
+  timer = setInterval(function () {
+    if (m === 0 && s === 0) {
+      clearInterval(timer);
+      timer = null;
+      return;
+    }
+    if (s === 0) {
+      if (m > 0) {
+        m--;
+        s = 59;
+      }
+    } else {
+      s--;
+    }
+    updateDisplay();
+  }, 1000);
+});
+
+stopEl.addEventListener("click", function () {
+  clearInterval(timer);
+  timer = null;
+});
+
+resetEl.addEventListener("click", function () {
+  clearInterval(timer);
+  timer = null;
+  m = 25;
+  s = 0;
+  updateDisplay();
+});
